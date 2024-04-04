b1c1251d13aa779c54207c0cbe67889db593376d
N.PARISSE <n.parisse95@gmail.com>
2024-04-04 10:11:29 +0200

    refactor dir name and config the lib

---
+++ Front-END/libcomponents/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        libraryTarget: 'umd',
+        library: 'libcomponents',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
