758c55d3f723be405ac567f99826957aa13930e9
Nathalie PARISSE <97409221+TangoAlphaN@users.noreply.github.com>
2024-04-05 00:56:26 +0200

    edit component

--- Front-END/libcomponents/src/components/BackBtn.js
+++ Front-END/libcomponents/src/components/BackBtn.js
@@ -5,11 +5,11 @@ function back(){
 	window.history.back();
 }
 
-export default function BackBtn() {
+export default function BackBtn({text}) {
 	return (
-		<button onClick={back} className="flex flex-row w-50 h-50 bg-gray-100 shadow rounded-lg py-2 px-8">
+		<button onClick={back} className="flex flex-row space-x-2 w-50 h-50 bg-gray-100 shadow rounded-3xl py-2 px-8 hover:bg-gray-300">
 			<Icon path={mdiArrowLeft} size={1}/>
-			<p className="m-auto inset-0 text-xl font-semibold leading-7 text-center text-gray-800">Retour</p>
+			<p className="m-auto inset-0 text-xl font-semibold text-center text-gray-800">{text}</p>
 		</button>
 	);
-}
+}    
