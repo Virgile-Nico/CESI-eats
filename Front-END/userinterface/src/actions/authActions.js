3eb6efe4d2d05a771d3abe4d4613639bb09a4d72
Nathalie PARISSE <97409221+TangoAlphaN@users.noreply.github.com>
2024-04-08 22:40:05 +0200

    add redux thunk and edit authAction with async

--- Front-END/userinterface/src/actions/authActions.js
+++ Front-END/userinterface/src/actions/authActions.js
@@ -1,9 +1,27 @@
+import axios from 'axios';
+
+export const login = (username, password) => {
+	return async dispatch => {
+		try {
+			// Perform your API call for login using Axios
+			const response = await axios.post('your-login-url', {
+				username,
+				password
+			});
+
+			const { accessToken, refreshToken } = response.data;
+
+			// Dispatch the loginSuccess action with the tokens
+			dispatch(loginSuccess(accessToken, refreshToken));
+		} catch (error) {
+			// Handle errors here
+			console.error('Login error:', error);
+		}
+	};
+};
+
 export const loginSuccess = (accessToken, refreshToken) => ({
 	type: 'LOGIN_SUCCESS',
 	accessToken,
 	refreshToken
-});
-
-export const logout = () => ({
-	type: 'LOGOUT'
 });
