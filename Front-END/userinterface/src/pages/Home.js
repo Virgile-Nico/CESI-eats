cd586fadab24226c95d709dbc37e5e9eae412fd5
N.PARISSE <n.parisse95@gmail.com>
2024-04-09 17:33:05 +0200

    add img

--- Front-END/userinterface/src/pages/Home.js
+++ Front-END/userinterface/src/pages/Home.js
@@ -4,7 +4,10 @@ export default function Home() {
     const isMobile = window.innerWidth <= 600;
     return (
         <main className="h-screen w-full flex flex-col items-center">
-            {!isMobile && (<HeaderDesktop articlesCount={0} />)}
+            {!isMobile && (
+                <HeaderDesktop articlesCount={0} />
+
+            )}
         </main>
     )
 }
