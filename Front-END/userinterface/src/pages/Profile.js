0b63c45454af05008ab7323b87e8f12740b2f23b
Nathalie PARISSE <97409221+TangoAlphaN@users.noreply.github.com>
2024-04-11 01:12:10 +0200

    edit profile page

--- Front-END/userinterface/src/pages/Profile.js
+++ Front-END/userinterface/src/pages/Profile.js
@@ -12,26 +12,28 @@ export default function Profile() {
     const navigate = useNavigate();
     const [addresses, setAddresses] = useState([]);
     const [payments, setPayments] = useState([]);
-    const [displayCount, setDisplayCount] = useState(7);
+    const [displayCountAddress, setDisplayCountAddress] = useState(3);
+    const [displayCountPayment, setDisplayCountPayment] = useState(2);
     const [isExpanded, setIsExpanded] = useState(false);
     const [isExpandedAddress, setIsExpandedAddress] = useState(false);
+    const isMobile = window.innerWidth <= 600;
 
     const handleShowMoreAddress = () => {
         setIsExpandedAddress(true);
-        setDisplayCount(addresses.length);
+        setDisplayCountAddress(addresses.length);
     };
     const handleShowMorePayment = () => {
         setIsExpanded(true);
-        setDisplayCount(payments.length);
+        setDisplayCountPayment(payments.length);
     };
 
     const handleShowLessAddress = () => {
         setIsExpandedAddress(false);
-        setDisplayCount(3);
+        setDisplayCountAddress(3);
     };
     const handleShowLessPayement = () => {
         setIsExpanded(false);
-        setDisplayCount(2);
+        setDisplayCountPayment(2);
     };
 
     useEffect(() => {
@@ -51,75 +53,74 @@ export default function Profile() {
         ])
     }, []);
 
-    /*const App = () => {
-        useEffect(() => {
-            axios.get('api-call')
-                .then(response => {
-                    setAddresses(response.data.name)
-                })
-                .catch(error => {
-                    console.error('There was an error!', error);
-                });
-        }, [])
 
-        useEffect(() => {
-            axios.get('api-call')
-                .then(response => {
-                    setPayements(response.data.name)
-                })
-                .catch(error => {
-                    console.error('There was an error!', error);
-                });
-        }, [])
-    }*/
+    useEffect(() => {
+        axios.get('api-call')
+            .then(response => {
+                setAddresses(response.data.name)
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, [])
+
+    useEffect(() => {
+        axios.get('api-call')
+            .then(response => {
+                setPayments(response.data.name)
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, [])
+
 
     return (
         <main className="flex flex-col items-center justify-between h-screen">
             <Avatar firstname={'Nathalie'} lastname={'parisse'} />
             <button className="bg-gray-200 rounded-lg p-2 text-primary-500 hover:bg-gray-100" onClick={() => navigate('/profile/edit')}>Modifier mon profil</button>
-            <div className="flex flex-col items-start h-full w-full space-y-2">
+            <div className={isMobile ? "flex flex-col items-start h-full w-full space-y-2" : "flex flex-row justify-between items-start h-full w-full mt-12 p-2"}>
                 <button className="flex flex-row space-x-4 bg-transparent hover:bg-gray-200 w-60 p-4" onClick={() => navigate('/orders')}>
                     <Icon path={mdiInvoiceList} size={1}/>
                     <span className="text-lg font-semibold text-gray-700">Mes commandes</span>
                 </button>
                 <div className="flex flex-col mx-4">
                     <h3 className="text-lg font-semibold text-gray-700">Mes adresses</h3>
-                    <div className="flex flex-row space-x-8">
+                    <div className={isMobile ? "flex flex-row space-x-8" : "flex flex-col space-x-8"} >
                         <div className="flex flex-col">
-                            {addresses.slice(0, displayCount).map((address, index) => (
+                            {addresses.slice(0, displayCountAddress).map((address, index) => (
                                 <Address key={index} street={address.street} zipCode={address.zipCode}
                                          city={address.city}/>
                             ))}
                         </div>
                         <div className="flex items-center justify-center">
                             {addresses.length > 3 && !isExpandedAddress && (
-                                <button className="bg-transparent flex flex-row items-center        p-2"
+                                <button className="bg-transparent flex flex-row items-center p-2"
                                         onClick={handleShowMoreAddress}>
                                     <div
                                         className="place-content-center justify-center p-4 rounded-xl ">
                                         <Icon path={mdiPlusBox} size={1}/>
                                     </div>
-                                    <h5 className="text-center text-xs md:text-sm font-semibold">Afficher plus</h5>
                                 </button>
                             )}
                             {isExpandedAddress && (
-                                <button className="bg-transparent flex flex-row items-center        p-2"
+                                <button className="bg-transparent flex flex-row items-center p-2"
                                         onClick={handleShowLessAddress}>
                                     <div
                                         className="place-content-center justify-center p-4 rounded-xl ">
                                         <Icon path={mdiMinusBox} size={1}/>
                                     </div>
-                                    <h5 className="text-center text-xs md:text-sm font-semibold">Afficher moins</h5>
                                 </button>
                             )}
                         </div>
                     </div>
                 </div>
+
                 <div className="flex flex-col mx-4">
                     <h3 className="text-lg font-semibold text-gray-700">Mes moyens de paiements</h3>
-                    <div className="flex flex-row space-x-8">
+                    <div className={isMobile ? "flex flex-row space-x-8" : "flex flex-col space-x-8"}>
                         <div className="flex flex-col">
-                            {payments.slice(0, displayCount).map((payment, index) => (
+                            {payments.slice(0, displayCountPayment).map((payment, index) => (
                                 <CardPayment key={index} cardNumber={payment.cardNumber} username={payment.username} cvc={payment.cvc} date={payment.date}/>
                             ))}
                         </div>
@@ -131,7 +132,6 @@ export default function Profile() {
                                         className="place-content-center justify-center p-4 rounded-xl ">
                                         <Icon path={mdiPlusBox} size={1}/>
                                     </div>
-                                    <h5 className="text-center text-xs md:text-sm font-semibold">Afficher plus</h5>
                                 </button>
                             )}
                             {isExpanded && (
@@ -141,7 +141,6 @@ export default function Profile() {
                                         className="place-content-center justify-center p-4 rounded-xl ">
                                         <Icon path={mdiMinusBox} size={1}/>
                                     </div>
-                                    <h5 className="text-center text-xs md:text-sm font-semibold">Afficher moins</h5>
                                 </button>
                             )}
                         </div>
